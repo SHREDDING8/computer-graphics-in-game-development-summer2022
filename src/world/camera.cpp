22a80b898891a64dee4c47a01f284a4d47992d5e
Orillio <kopn48@mail.ru>
2022-05-27 15:16:52 +0300

    // TODO: Lab 1.04. Setup an instance of camera `cg::world::camera` class in `cg::renderer::rasterization_renderer`

--- src/world/camera.cpp
+++ src/world/camera.cpp
@@ -40,27 +40,46 @@ void cg::world::camera::set_angle_of_view(float in_aov)
 void cg::world::camera::set_height(float in_height)
 {
 	// TODO: Lab 1.04. Implement `cg::world::camera` class
+    height = in_height;
 }
 
 void cg::world::camera::set_width(float in_width)
 {
 	// TODO: Lab 1.04. Implement `cg::world::camera` class
+    width = in_width;
+    aspect_ratio = width/height;
+
+
 }
 
 void cg::world::camera::set_z_near(float in_z_near)
 {
 	// TODO: Lab 1.04. Implement `cg::world::camera` class
+    z_near = in_z_near
 }
 
 void cg::world::camera::set_z_far(float in_z_far)
 {
 	// TODO: Lab 1.04. Implement `cg::world::camera` class
+    z_far = in_z_far;
 }
 
 const float4x4 cg::world::camera::get_view_matrix() const
 {
 	// TODO: Lab 1.04. Implement `cg::world::camera` class
-    return float4x4{};
+    float3 up{0.f,1.f,0.f};
+    float3  eye = position+get_direction();
+
+    float3 z_axis = normalize(position-eye);
+    float3 x_axis = normalize(cross(up,z_axis));
+    float3 y_axis = cross(z_axis,x_axis);
+
+    return float4x4{
+            {x_axis.x,y_axis,z_axis,0},
+            {x_axis.y,y_axis,z_axis,0},
+            {x_axis.z,y_axis,z_axis,0},
+            {x_axis,y_axis,z_axis,0},
+    };
 }
 
 #ifdef DX12
@@ -101,21 +120,21 @@ const float3 cg::world::camera::get_direction() const
 const float3 cg::world::camera::get_right() const
 {
 	// TODO: Lab 1.04. Implement `cg::world::camera` class
-    return float3{};
+    return linalg::cross(get_direction(),float3{0,1,0});
 }
 
 const float3 cg::world::camera::get_up() const
 {
 	// TODO: Lab 1.04. Implement `cg::world::camera` class
-    return float3{};
+    return linalg::cross(get_right(),get_direction());
 }
 const float camera::get_theta() const
 {
 	// TODO: Lab 1.04. Implement `cg::world::camera` class
-    return float{};
+    return theta;
 }
 const float camera::get_phi() const
 {
 	// TODO: Lab 1.04. Implement `cg::world::camera` class
-    return float{};
+    return phi;
 }
--- src/world/model.cpp
+++ src/world/model.cpp
@@ -14,39 +14,90 @@ cg::world::model::model() {}
 
 cg::world::model::~model() {}
 
-void cg::world::model::load_obj(const std::filesystem::path& model_path)
-{
-	// TODO: Lab 1.03. Using `tinyobjloader` implement `load_obj`, `get_vertex_buffers`, `get_index_buffers` methods of `cg::world::model` class
-}
-
-
-const std::vector<std::shared_ptr<cg::resource<cg::vertex>>>&
-cg::world::model::get_vertex_buffers() const
-{
-	// TODO: Lab 1.03. Using `tinyobjloader` implement `load_obj`, `get_vertex_buffers`, `get_index_buffers` methods of `cg::world::model` class
-return std::vector<std::shared_ptr<cg::resource<cg::vertex>>>();
-}
-
-
-const std::vector<std::shared_ptr<cg::resource<unsigned int>>>&
-cg::world::model::get_index_buffers() const
-{
+void cg::world::model::load_obj(const std::filesystem::path &model_path) {
     // TODO: Lab 1.03. Using `tinyobjloader` implement `load_obj`, `get_vertex_buffers`, `get_index_buffers` methods of `cg::world::model` class
-    return std::vector<std::shared_ptr<cg::resource<unsigned int>>>();
+    tinyobj::ObjReaderConfig readerConfig;
+    readerConfig.mtl_search_path = model_path.parent_path().string();
+    readerConfig.triangulate = true;
+
+    tinyobj::ObjReader reader;
+    if (!reader.ParseFromFile(model_path.string(), readerConfig)) {
+        if (!reader.Error().empty()) {
+            THROW_ERROR(reader.Error());
+        }
+    }
+    auto &attrib = reader.GetAttrib();
+    auto &shapes = reader.GetShapes();
+    auto &materials = reader.GetMaterials();
+    textures.resize(shapes.size());
+    for (size_t s = 0; s < shapes.size(); s++) {
+        size_t index_offset = 0;
+        unsigned int vertex_buffer_size = 0;
+        unsigned int index_buffer_size = 0;
+        std::map<std::tuple<int, int, int>, unsigned int> index_map;
+        const auto &mesh = shapes[s].mesh;
+
+        for (size_t f = 0; f < mesh.num_face_vertices.size(); f++) {
+            int fv = mesh.num_face_vertices[f];
+            for (size_t v = 0; v < fv; v++) {
+                tinyobj::index_t idx = mesh.indices[index_offset + v];
+                auto idx_tuple = std::make_tuple(
+                        idx.vertex_index,
+                        idx.normal_index,
+                        idx.texcoord_index
+                );
+
+                if(index_map.count(idx_tuple) == 0){
+                    index_map[idx_tuple] = vertex_buffer_size;
+                    vertex_buffer_size++;
+                }
+                index_buffer_size++;
+
+            }
+            index_offset+=fv;
+
+        }
+        vertex_buffers.push_back(
+                std::make_shared<cg::resource<cg::vertex>>(
+                        vertex_buffer_size
+                        )
+                );
+        index_buffers.push_back(
+                std::make_shared<cg::resource<unsigned int>>(
+                        index_buffer_size
+                        )
+                );
+        if(!materials[mesh.material_ids[0]].diffuse_texname.empty()){
+            textures[s]= model_path.parent_path()/materials[mesh.material_ids[0]].diffuse_texname;
+        }
+    }
+
+}
+
+
+const std::vector<std::shared_ptr<cg::resource<cg::vertex>>> &
+cg::world::model::get_vertex_buffers() const {
+    // TODO: Lab 1.03. Using `tinyobjloader` implement `load_obj`, `get_vertex_buffers`, `get_index_buffers` methods of `cg::world::model` class
+    return vertex_buffers;
+}
+
+
+const std::vector<std::shared_ptr<cg::resource<unsigned int>>> &
+cg::world::model::get_index_buffers() const {
+    // TODO: Lab 1.03. Using `tinyobjloader` implement `load_obj`, `get_vertex_buffers`, `get_index_buffers` methods of `cg::world::model` class
+    return index_buffers;
 }
 
 std::vector<std::filesystem::path>
-cg::world::model::get_per_shape_texture_files() const
-{
-	return textures;
+cg::world::model::get_per_shape_texture_files() const {
+    return textures;
 }
 
 
-const float4x4 cg::world::model::get_world_matrix() const
-{
-	return float4x4{
-			{1, 0, 0, 0},
-			{0, 1, 0, 0},
-			{0, 0, 1, 0},
-			{0, 0, 0, 1}};
+const float4x4 cg::world::model::get_world_matrix() const {
+    return float4x4{
+            {1, 0, 0, 0},
+            {0, 1, 0, 0},
+            {0, 0, 1, 0},
+            {0, 0, 0, 1}};
 }
